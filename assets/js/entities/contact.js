8221995ad4b9defb21914c461058833c6fe61132
Dmitri <orldm@mail.ru>
2016-05-10 13:46:54 +0300

    add api

--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -11,4 +11,26 @@ ContactManager.module('Entities', function(Entities, ContactManager, Backbone, M
     comparator: 'firstName'
   });
 
+  var contacts;
+
+  var initializeContacts = function() {
+    contacts = new Entities.ContactCollection([
+      {id: 1, firstName: 'Alice', lastName: 'Arten', phoneNumber: '333-333'},
+      {id: 2, firstName: 'Bob', lastName: 'Brigham', phoneNumber: '222-222'}
+    ]);
+  };
+
+  var API = {
+    getContactEntities: function(){
+      if(contacts === undefined) {
+        initializeContacts();
+      }
+      return contacts;
+    }
+  };
+
+  ContactManager.reqres.setHandler('contact:entities', function() {
+    return API.getContactEntities();
+  });
+
 });
