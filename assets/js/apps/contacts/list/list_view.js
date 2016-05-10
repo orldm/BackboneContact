ace8f9ddfa3da77c4b877c48ebf70ec780545b2c
Dmitri <orldm@mail.ru>
2016-05-10 16:37:42 +0300

    refactor fadeout

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -18,7 +18,10 @@ ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbon
       this.trigger('contact:delete', this.model);
     },
     remove: function() {
-      this.$el.fadeOut();
+      var self = this;
+      this.$el.fadeOut(function() {
+        Marionette.ItemView.prototype.remove.call(self);
+      });
     }
   });
 
