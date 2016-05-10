9bfb7dd3ea7edb99f44ae67565ab4a6666dcb83d
Dmitri <orldm@mail.ru>
2016-05-10 14:19:25 +0300

    refactor app and index

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,9 @@
 var ContactManager = new Marionette.Application();
+
 ContactManager.addRegions({
   mainRegion: "#main-region"
+});
+
+ContactManager.on('start', function(){
+  ContactManager.ContactsApp.List.Controller.listContacts();
 });
